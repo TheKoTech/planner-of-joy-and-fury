f159a39ae6c4328a1c4883c82494e3fa2d8f7f83
Welnyr <thekotech@gmail.com>
2024-09-06 00:06:28 +0500

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import pluginJs from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default [
+	{ files: ['**/*.{js,mjs,cjs,ts}'] },
+
+	{
+		files: ['**/*.js'],
+		languageOptions: {
+			sourceType: 'commonjs',
+		},
+	},
+
+	{
+		languageOptions: {
+			globals: globals.browser,
+		},
+	},
+
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+]
