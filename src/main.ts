49cfae6d7ff74208065ff46319dd8788fb14efa6
Welnyr <thekotech@gmail.com>
2024-09-06 00:22:08 +0500

    added bot template

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
+import { Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 console.log('hello world!')
+
+const bot = new Telegraf(process.env.BOT_TOKEN!)
+bot.start(ctx => ctx.reply('Welcome'))
+bot.help(ctx => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), ctx => ctx.reply('👍'))
+bot.hears('hi', ctx => ctx.reply('Hey there'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
